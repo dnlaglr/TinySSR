529d88736b1708f6f7c2ddb26b2543a74524b0c3
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-01-28 07:52:31 -0800

    Get SSR server working

--- tiny-ssr/src/server/index.tsx
+++ tiny-ssr/src/server/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { renderToPipeableStream } from "react-dom/server";
 import { readdirSync } from "fs";
-import { createServer } from "http";
+import { createServer, IncomingMessage } from "http";
 import { join } from "path";
 
-function startServer(port: number) {
-  const pagesDir = join(process.cwd(), "build/pages");
+interface tinyMessage extends IncomingMessage {
+  query?: any
+}
+
+function startServer() {
+  const pagesDir = join(process.cwd(), "dist/pages");
   const pages = readdirSync(pagesDir).map(
     (page) => page.split(".")[0]
   );
 
-  const server = createServer(async (req, res) => {
+  const server = createServer(async (req: tinyMessage, res) => {
     const urlParts = req.url && req.url !== "/" ? req.url.split("/") : ["", "index"];
     const page = urlParts[1];
 
@@ -18,13 +22,18 @@ function startServer(port: number) {
       const file = await import(`file://${join(pagesDir, `${page}.js`)}`);
       const Component = file.default;
 
+      var props = {};
+      if (file.getServerSideProps) {
+        props = await file.getServerSideProps({ query: req.query });
+      }
+
       const { pipe } = renderToPipeableStream(
         <body>
           <div id="root">
-            <Component />
+            <Component {...props} />
           </div>
         </body>,
-        { bootstrapScripts: ["/index.js"] }
+        { bootstrapScripts: ["http://localhost:3000/src/index.js"] }
       );
 
       pipe(res);
@@ -34,8 +43,8 @@ function startServer(port: number) {
     }
   });
 
-  server.listen(port, () => {
-    console.log("Server is running at http://localhost:3000");
+  server.listen(5000, () => {
+    console.log("Server is running at http://localhost:5000");
     console.log(`Reading pages from ${pagesDir}`);
     console.log(`Available pages: ${pages}`);
   });
